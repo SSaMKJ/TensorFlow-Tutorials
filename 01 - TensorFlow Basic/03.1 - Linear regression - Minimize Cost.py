857247fcc8296a3e4b779fc05c3678880782eaa9
김진삼 <ssamkj@martmoa.com>
2017-02-13 21:55:29 +0900

    Lec 3강 코스트를 줄이기 실습 소스

---
+++ 01 - TensorFlow Basic/03.1 - Linear regression - Minimize Cost.py
@@ -0,0 +1,33 @@
+
+import tensorflow as tf
+
+x_data = [1., 2., 3.]
+y_data = [1., 2., 3.]
+
+W = tf.Variable(tf.random_uniform([1], -10.0, 10.0))
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+
+hypothesis = W*X
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+descent = W - tf.mul(0.1, tf.reduce_mean(tf.mul((tf.mul(W, X) - Y), X)))
+
+update = W.assign(descent)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(200):
+    sess.run(update, feed_dict={X:x_data, Y:y_data})
+    print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W))
+    if sess.run(cost, feed_dict={X:x_data, Y:y_data}) == 0:
+        break
+
+print('실제 값이 어떻게 나오나 보자 = ', sess.run(hypothesis, feed_dict={X:5}))
+print('실제 값이 어떻게 나오나 보자 = ', sess.run(hypothesis, feed_dict={X:50}))
